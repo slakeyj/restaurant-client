0cce23e701284a333484fe312d927b591082a017
Susanna Lakey <susannalakey@gmail.com>
2021-02-22 13:31:08 -0800

    added average rating and count

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 import { useHistory } from 'react-router-dom';
+import StarRating from './StarRating';
 
 const RestaurantList = () => {
   const { restaurants, setRestaurants } = useContext(RestaurantsContext);
@@ -65,7 +66,12 @@ const RestaurantList = () => {
                   <td>{restaurant.name}</td>
                   <td>{restaurant.location}</td>
                   <td>{'$'.repeat(restaurant.price_range)}</td>
-                  <td>Rating</td>
+                  <td>
+                    <StarRating rating={restaurant.average_rating} />
+                    <span className='text-warning ml-1'>
+                      {restaurant.count ? `(${restaurant.count})` : '(0)'}
+                    </span>
+                  </td>
                   <td>
                     <button
                       onClick={e => handleUpdate(e, restaurant.id)}
--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#faec23',
+      main: '#ffc107',
     },
   },
 });
