9a28db583f54dbaad0ecacfda27435f8b1b086bd
Susanna Lakey <susannalakey@gmail.com>
2021-02-24 13:47:05 -0800

    updating urls

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
+// NODE_ENV = 'development'
+// NODE_ENV = 'production'
+
+// if we are in production, use baseurl = /api/v1/restaurants
+// if we are in development, use baseurl = http://localhost:3005/api/v1/restaurants
+
+const baseURL =
+  process.env.NODE_ENV === 'prudction'
+    ? 'api/v1/restaurants'
+    : 'http://localhost:3005/api/v1/restaurants';
+
 export default axios.create({
-  baseURL: 'http://localhost:3005/api/v1/restaurants',
+  baseURL,
 });
